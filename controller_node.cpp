64542ad08742233a1c067d99b73b71a23dd20cbb
sebasqr22 <sebastianqr.2208@gmail.com>
2021-06-23 21:43:40 -0600

    Se agrega la parte de binario
    Co-authored-by: MArcos Gonzalez <quigonar@gmail.com>

--- controller_node.cpp
+++ controller_node.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <bitset>
 
 using namespace std;
 
 void toBinary(){
+    int counter = 0;
     ifstream toConvert;
     ofstream destinationFile;
-    string line;
+    string content;
+    string binaryString = "";
     toConvert.open("test.txt", ios::binary);
     destinationFile.open("test2.txt", ios::binary);
-    while(!toConvert.eof()){
-        getline(toConvert, line);
-        cout << line << endl;
-        destinationFile.write(line.c_str(), line.length());
+    for (int i=0; toConvert.eof()!= true; i++)
+        content += toConvert.get();
+
+    for (char& _char : content) {
+        binaryString += bitset<8>(_char).to_string() + " ";
+        counter++;
     }
+    cout << binaryString << endl;
+    destinationFile.write(binaryString.c_str(), binaryString.length());
+
     toConvert.close();
     destinationFile.close();
+
+    cout << "Cantidad de bits de 8: " << counter << endl;
+    
+    
+}
+void toText(){
+
 }
 int main(){
     toBinary();
